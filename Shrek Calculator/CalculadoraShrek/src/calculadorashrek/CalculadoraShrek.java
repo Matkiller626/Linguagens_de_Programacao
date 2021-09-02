bee11d7a119f2da7828bf95edea4ed367aea8631
Matheus Camargo <84470319+Matkiller626@users.noreply.github.com>
2021-09-01 21:14:20 -0300

    Add files via upload

---
+++ Shrek Calculator/CalculadoraShrek/src/calculadorashrek/CalculadoraShrek.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadorashrek;
+
+/**
+ *
+ * @author Matheus Camargo
+ */
+public class CalculadoraShrek {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        UI_Calculadora calc = new UI_Calculadora();
+        calc.setVisible(true);// TODO code application logic here
+    }
+    
+}
